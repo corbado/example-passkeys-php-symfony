a3b349a333a43ab00c66bda37b50d2aaf1ddac12
Nicolai Stein <nicolai_stein@outlook.de>
2022-12-07 17:54:38 +0100

    Integrated php client

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use Corbado\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,7 +9,6 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class BackendController extends AbstractController
 {
-
     /**
      * @Route("/api/loginInfo", name="loginInfo", methods="GET")
      */
@@ -39,33 +39,15 @@ class BackendController extends AbstractController
     public function sessionToken(Request $request, SessionInterface $session): Response
     {
         $token = $request->query->get('sessionToken');
-
-        //Prepare Corbado request header
-        $encoded = base64_encode(sprintf("%s:%s", $_ENV['PROJECT_ID'], $_ENV['API_SECRET']));
-        $authentication = "Basic $encoded";
         $useragent = $request->headers->get('User-Agent');
         $remoteAddress = system("curl -s ipv4.icanhazip.com");
 
-        //Prepare Corbado request body
-        $body = [
-            'token' => $token,
-            'clientInfo' => [
-                "remoteAddress" => $remoteAddress,
-                "userAgent" => $useragent
-            ],
-        ];
-
-        //Execute Corbado request
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://api.corbado.com/v1/sessions/verify");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json', "Authorization: $authentication"));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
+        $apiClient = new Client("https://api.corbado.com/v1", $_ENV["PROJECT_ID"], $_ENV["API_SECRET"]);
+        $result = $apiClient->widget()->sessionVerify($token, $remoteAddress, $useragent);
+        $response = $result->getData()->getUserData();
 
         //Parse json response from Corbado request
-        $data = json_decode($response, true)['data'];
-        $userData = json_decode($data['userData'], true);
+        $userData = json_decode($response, true);
         $username = $userData['username'];
         $userFullName = $userData['userFullName'];
 
