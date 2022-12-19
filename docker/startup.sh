5db0c03b4be232071e050ddc49e4f07a5605a5ab
Nicolai Stein <nicolai_stein@outlook.de>
2022-12-19 13:15:16 +0100

    Added migration on startup and healthcheck

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,6 +5,8 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
     cd var/www/html
+    
+    symfony console doctrine:migrations:migrate --no-interaction
     symfony console doctrine:fixtures:load --no-interaction
 else
     echo "-- Not first container startup --"
