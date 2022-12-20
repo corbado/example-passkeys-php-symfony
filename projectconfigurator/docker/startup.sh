efe9007f20b435bfe40ca2f8f5f3f43178791521
Nicolai Stein <nicolai_stein@outlook.de>
2022-12-20 15:13:14 +0100

    Get tunnels from ngrok dev panel

--- projectconfigurator/docker/startup.sh
+++ projectconfigurator/docker/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/src/app/projectconfigurator/docker/wait-for-it.sh ngrok:4551 -s -t 300 -- python /usr/src/app/projectconfigurator/main.py
+/usr/src/app/projectconfigurator/docker/wait-for-it.sh ngrok:4551 -s -t 1000 -- python /usr/src/app/projectconfigurator/main.py
--- projectconfigurator/main.py
+++ projectconfigurator/main.py
@@ -1,10 +1,16 @@
+import requests
 
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print("TEST HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+import time
+
+
+
+
+if __name__ == "__main__":
+
+    # Do stuff with your driver
+    print("Making request...")
+
+    response = requests.get("http://ngrok:4551/api/tunnels")
+    print("response")
+    print(response.status_code)
+    print(response.json())
