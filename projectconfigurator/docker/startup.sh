85f7cb00359635f55fd2708abe82d8baadea9b55
Nicolai Stein <nicolai_stein@outlook.de>
2022-12-20 19:55:07 +0100

    Fixed docker

--- projectconfigurator/docker/startup.sh
+++ projectconfigurator/docker/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-/usr/src/app/projectconfigurator/docker/wait-for-it.sh ngrok:4551 -s -t 10000 -- python /usr/src/app/projectconfigurator/main.py
+/usr/src/app/projectconfigurator/docker/wait-for-it.sh ngrok:4551 -s -t 10000
+/usr/src/app/projectconfigurator/docker/wait-for-it.sh symfony:80 -s -t 10000 
+python /usr/src/app/projectconfigurator/main.py
--- symfony/docker/startup.sh
+++ symfony/docker/startup.sh
@@ -4,7 +4,7 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    cd var/www/html
+    cd var/www/html/symfony
     
     symfony console doctrine:migrations:migrate --no-interaction
     symfony console doctrine:fixtures:load --no-interaction
