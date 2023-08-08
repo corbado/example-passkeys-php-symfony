5e5bdb512566d2b3664c534c389f78469aa958ef
Nicolai Stein <nicolai_stein@outlook.de>
2023-08-08 14:09:51 +0200

    remove setup api cmd and params

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,7 +3,6 @@
 composer install
 php bin/console -n doctrine:migrations:migrate
 php bin/console -n doctrine:fixtures:load
-php bin/console app:setup-api
 
 corbado login --projectID $PROJECT_ID --cliSecret $CLI_SECRET
 cd public/ && php -S 0.0.0.0:8000 &
